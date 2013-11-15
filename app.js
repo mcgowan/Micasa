b0affc9b3b3dbe4ec358ceb38229b4343d2ec874
Michael McGowan <mike.mcgowan@gmail.com>
2013-11-15 14:02:14 -0800

    Check in, shelve.

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 var express = require('express');
 
-var craigslist = require('./routes/listing/craigslist'),
+var list = require('./routes/listing'),
+    site = require('./routes/site'),
     http = require('http'),
     path = require('path');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use("/js", express.static(__dirname + '/site/js'));
 
 // development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
+// if ('development' == app.get('env')) {
+//     app.use(express.errorHandler());
+// }
 
-//app.get('/', routes.index);
-app.get('/listing', craigslist.listing);
-app.get('/listing/:id', craigslist.post);
+
+app.get('/', site.index);
+app.get('/site', site.index);
+app.get('/listing', list.listing);
+app.get('/listing/:id', list.posting);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
