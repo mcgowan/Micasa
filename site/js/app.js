f56955b0419afd3f0a29b62deed600fddaa8f754
mcgowan <mike.mcgowan@gmail.com>
2013-11-20 14:01:54 -0800

    Check in, shelve.

--- site/js/app.js
+++ site/js/app.js
@@ -1,1 +1,24 @@
-var micasaApp = angular.module('micasaApp', []);
+var micasaApp = angular.module('micasaApp', [])
+    .value('api', 'http://127.0.0.1:3000')
+.factory('listingsService', function ($http, $log, api) {
+    return {
+        getListings: function (criteria, callback) {
+            $http({ method: 'GET', url: '{0}/list?start={1}&query={2}&min={3}&max={4}'.format(api, criteria.start, criteria.keyword, criteria.min, criteria.max) }).
+				success(function (data, status, headers, config) {
+				    callback(data);
+				}).
+				error(function (data, status, headers, config) {
+				    $log.warn(data, status, headers, config);
+				});
+        },
+        getListing: function (id, callback) {
+            $http({ method: 'GET', url: '{0}/list/{1}'.format(api, id) }).
+				success(function (data, status, headers, config) {
+				    callback(data);
+				}).
+				error(function (data, status, headers, config) {
+				    $log.warn(data, status, headers, config);
+				});
+        }
+    };
+});
--- site/js/controllers/listingsController.js
+++ site/js/controllers/listingsController.js
@@ -1,52 +1,51 @@
 micasaApp.controller('listingsController', function listingsController($scope, listingsService) {
-	
-	$scope.criteria = {
-		keyword: '',
-		min: 1000,
-		max: 3000,
-		start: 0
-	};
 
-	listingsService.getListings($scope.criteria, function(data){
-		$scope.listings = data.listings;
-		_.each($scope.listings, function(listing){
-			listingsService.getListing(listing.id, function(data){
-				showPager = false;
-				listing.url = data.url;
-				listing.images = data.images;
-				listing.currentImage = listing.images[0];
-				listing.currentImageIndex = 0;
-			});
-		});
-	});
+    $scope.criteria = {
+        keyword: '',
+        min: 1000,
+        max: 3000,
+        start: 0
+    };
 
-	$scope.open = function(listing) {
-		window.open(listing.url);
-	}
+    $scope.search = function () {
+        $scope.listings = [];
+        listingsService.getListings($scope.criteria, function (data) {
+            $scope.listings = data.listings;
+            _.each($scope.listings, function (listing) {
+                listingsService.getListing(listing.id, function (data) {
+                    showPager = false;
+                    listing.url = data.url;
+                    listing.images = data.images;
+                    listing.currentImage = listing.images[0];
+                    listing.currentImageIndex = 0;
+                });
+            });
+        });
+    };
 
-	$scope.hover = function(listing) {
-	    if (listing.images.length > 1)
-		    listing.showPager = ! listing.showPager;
-	};	
+    $scope.open = function (listing) {
+        window.open(listing.url);
+    }
 
-	$scope.pageLeft = function(listing) {
-		if (listing.currentImageIndex !== 0) {
-			listing.currentImageIndex--;
-			listing.currentImage = listing.images[listing.currentImageIndex];
-		}
-	};	
+    $scope.hover = function (listing) {
+        if (listing.images.length > 1)
+            listing.showPager = !listing.showPager;
+    };
 
-	$scope.pageRight = function(listing) {
-		if (listing.currentImageIndex !== (listing.images.length - 1)) {
-			listing.currentImageIndex++;
-			listing.currentImage = listing.images[listing.currentImageIndex];
-		}
-	};	
+    $scope.pageLeft = function (listing) {
+        if (listing.currentImageIndex !== 0) {
+            listing.currentImageIndex--;
+            listing.currentImage = listing.images[listing.currentImageIndex];
+        }
+    };
 
-	$scope.search = function() {
-		//alert($scope.keyword);
+    $scope.pageRight = function (listing) {
+        if (listing.currentImageIndex !== (listing.images.length - 1)) {
+            listing.currentImageIndex++;
+            listing.currentImage = listing.images[listing.currentImageIndex];
+        }
+    };
 
-		$scope.listings = [];
-	};
+    $scope.search();
 
 });
